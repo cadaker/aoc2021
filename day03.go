2de1329f3a7591c3a35702aa27fe49dba42018be
Christian Adåker <552387+cadaker@users.noreply.github.com>
2021-12-03 10:37:03 +0100

    Day 03 - refactor to prepare for part 2

--- day03.go
+++ day03.go
@@ -6,34 +6,44 @@ import (
 	"os"
 )
 
-func main() {
-	totalLines := 0
-	zeroCounts := make(map[int]int)
-
-	input := bufio.NewScanner(os.Stdin)
-
-	for input.Scan() {
-		totalLines++
-		line := input.Text()
-		for i := 0; i < len(line); i++ {
-			if line[i] == '0' {
-				zeroCounts[i]++
+func countZeros(lines []string) map[int]int {
+	ret := make(map[int]int)
+	for _, line := range lines {
+		for col, ch := range line {
+			if ch == '0' {
+				ret[col]++
 			}
 		}
 	}
+	return ret
+}
 
+func powerConsumption(zeroCounts map[int]int, wordCount int) int {
 	wordSize := len(zeroCounts)
 	gamma := 0
 	epsilon := 0
 	for i := 0; i < wordSize; i++ {
 		gamma *= 2
 		epsilon *= 2
-		if zeroCounts[i] > totalLines/2 {
+		if zeroCounts[i] > wordCount/2 {
 			epsilon += 1
 		} else {
 			gamma += 1
 		}
 	}
+	return gamma * epsilon
+}
 
-	fmt.Println(epsilon * gamma)
+func main() {
+	lines := make([]string, 0)
+
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		lines = append(lines, input.Text())
+	}
+
+	zeroCounts := countZeros(lines)
+
+	fmt.Println(powerConsumption(zeroCounts, len(lines)))
 }
