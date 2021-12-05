4a277e7915b39fc9a7d46ce8de578997217eecc2
Christian Adåker <552387+cadaker@users.noreply.github.com>
2021-12-05 08:58:52 +0100

    Day 05 part 2

--- day05.go
+++ day05.go
@@ -94,52 +94,46 @@ func imax(x int, y int) int {
 	}
 }
 
-func xrange(y int, x0 int, x1 int) []Point {
-	var points []Point
-	for x := x0; x <= x1; x++ {
-		points = append(points, Point{x, y})
-	}
-	return points
-}
-
-func yrange(x int, y0 int, y1 int) []Point {
-	var points []Point
-	for y := y0; y <= y1; y++ {
-		points = append(points, Point{x, y})
-	}
-	return points
-}
-
-func simpleIntersects(l0 Line, l1 Line) []Point {
-	if isHorizontal(l0) && isHorizontal(l1) && l0.P0.Y == l1.P0.Y {
-		if l0.P0.X > l1.P0.X {
-			return simpleIntersects(l1, l0)
-		} else {
-			x0 := imax(l0.P0.X, l1.P0.X)
-			x1 := imin(l0.P1.X, l1.P1.X)
-			return xrange(l0.P0.Y, x0, x1)
-		}
-	} else if isVertical(l0) && isVertical(l1) && l0.P0.X == l1.P0.X {
-		if l0.P0.Y > l1.P0.Y {
-			return simpleIntersects(l1, l0)
-		} else {
-			y0 := imax(l0.P0.Y, l1.P0.Y)
-			y1 := imin(l0.P1.Y, l1.P1.Y)
-			return yrange(l0.P0.X, y0, y1)
-		}
-	} else if isHorizontal(l0) && isVertical(l1) {
-		return simpleIntersects(l1, l0)
-	} else if isVertical(l0) && isHorizontal(l1) {
-		x0 := l0.P0.X
-		y1 := l1.P0.Y
-		if l1.P0.X <= x0 && x0 <= l1.P1.X && l0.P0.Y <= y1 && y1 <= l0.P1.Y {
-			return []Point{Point{x0, y1}}
-		} else {
-			return []Point{}
-		}
+func iabs(x int) int {
+	if x < 0 {
+		return -x
 	} else {
-		return []Point{}
+		return x
 	}
+}
+
+func isign(x int) int {
+	if x > 0 {
+		return 1
+	} else if x < 0 {
+		return -1
+	} else {
+		return 0
+	}
+}
+
+func intersects(l0 Line, l1 Line) []Point {
+	len0 := imax(iabs(l0.P1.X - l0.P0.X), iabs(l0.P1.Y - l0.P0.Y))
+	dx0 := isign(l0.P1.X - l0.P0.X)
+	dy0 := isign(l0.P1.Y - l0.P0.Y)
+	len1 := imax(iabs(l1.P1.X - l1.P0.X), iabs(l1.P1.Y - l1.P0.Y))
+	dx1 := isign(l1.P1.X - l1.P0.X)
+	dy1 := isign(l1.P1.Y - l1.P0.Y)
+
+	points := []Point{}
+
+	for k0 := 0; k0 <= len0; k0++ {
+		x0 := l0.P0.X + k0*dx0
+		y0 := l0.P0.Y + k0*dy0
+		for k1 := 0; k1 <= len1; k1++ {
+			x1 := l1.P0.X + k1*dx1
+			y1 := l1.P0.Y + k1*dy1
+			if x0 == x1 && y0 == y1 {
+				points = append(points, Point{x0, y0})
+			}
+		}
+	}
+	return points
 }
 
 func main() {
@@ -150,15 +144,18 @@ func main() {
 	}
 
 	allIntersections := map[Point]bool{}
+	allAxisAlignedIntersections := map[Point]bool{}
 	for i, line0 := range input {
 		for _, line1 := range input[i+1:] {
-			if isAxisAligned(line0) && isAxisAligned(line1) {
-				intersections := simpleIntersects(line0, line1)
-				for _, p := range intersections {
-					allIntersections[p] = true
+			intersections := intersects(line0, line1)
+			for _, p := range intersections {
+				allIntersections[p] = true
+				if isAxisAligned(line0) && isAxisAligned(line1) {
+					allAxisAlignedIntersections[p] = true
 				}
 			}
 		}
 	}
+	fmt.Println(len(allAxisAlignedIntersections))
 	fmt.Println(len(allIntersections))
 }
