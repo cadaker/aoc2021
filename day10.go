9c81962277720f743b9b51c6b3964a0f650f6974
Christian Adåker <552387+cadaker@users.noreply.github.com>
2021-12-12 17:17:14 +0100

    Day 10 part 2

--- day10.go
+++ day10.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
 func parseInput() []string {
@@ -15,11 +16,12 @@ func parseInput() []string {
 	return lines
 }
 
-// Returns the first invalid character, or 0 if the line is ok
-func checkLine(line string) uint8 {
+// Returns a pair of the first invalid character (or 0), followed by the
+// paren stack at that point, or at the end
+func checkLine(line string) (uint8, []uint8) {
 	closer := map[uint8]uint8{'(':')', '{':'}', '[':']', '<':'>'}
 	if len(line) == 0 {
-		return 0
+		return 0, []uint8{}
 	}
 	var stack []uint8
 	for i := 0; i < len(line); i++ {
@@ -29,13 +31,27 @@ func checkLine(line string) uint8 {
 		} else {
 			opener := stack[len(stack)-1]
 			if len(stack) == 0 || line[i] != closer[opener] {
-				return line[i]
+				return line[i], stack
 			} else {
 				stack = stack[0:len(stack)-1]
 			}
 		}
 	}
-	return 0
+	return 0, stack
+}
+
+func computeCompletionScore(stack []uint8) int {
+	scores := map[uint8]int{'(': 1, '[':2, '{':3, '<':4}
+	score := 0
+	for i := len(stack) - 1; i >= 0; i-- {
+		score = score * 5 + scores[stack[i]]
+	}
+	return score
+}
+
+func getMiddleScore(scores []int) int {
+	sort.Ints(scores)
+	return scores[len(scores)/2]
 }
 
 func main() {
@@ -43,11 +59,15 @@ func main() {
 
 	scoring := map[uint8]int{')':3, ']':57, '}':1197, '>':25137}
 	totalScore := 0
+	var completionScores []int
 	for _, line := range lines {
-		check := checkLine(line)
-		if scoring[check] > 0 {
+		check, stack := checkLine(line)
+		if check == 0 {
+			completionScores = append(completionScores, computeCompletionScore(stack))
+		} else if scoring[check] > 0 {
 			totalScore += scoring[check]
 		}
 	}
 	fmt.Println(totalScore)
+	fmt.Println(getMiddleScore(completionScores))
 }
